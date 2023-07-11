c2ebd3628ce21723ab8515af44dd7e6a54cebcbe
manoj-shetty <manojs231@outlook.com>
2023-07-11 21:58:51 +0530

    second commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,27 +15,12 @@ const Navbar = () => {
                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">Action</a></li>
-                                    <li><a className="dropdown-item" href="/">Another action</a></li>
-                                    <li><hr className="dropdown-divider"/></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
+                                <a className="nav-link" href="/about">About</a>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                        <div>
+                            <button disabled={true} className="btn btn-primary">Your Balance: 0</button>
+                        </div>
                     </div>
                 </div>
             </nav>
